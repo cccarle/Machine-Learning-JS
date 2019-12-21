55c0f7968d2bd6895c7e015c99d4320d7f992f55
cccarle <carleinarsson@live.se>
2019-12-21 13:20:27 +0100

    added standard devitation to all flowers property

--- model/NaiveBayes.js
+++ model/NaiveBayes.js
@@ -1,18 +1,50 @@
 // x = values, y = labels
 const fit = (x, y) => {
-  let arr = []
+  let flowerTrainingData = []
 
-  y.map(label => {
-    arr.push(x.filter(x => x.species == label))
-  })
-
-  calculateMeanForEveryFlowerProperty(arr)
-
-  console.log(arr[0])
+  dividedFlowerSetByCategory(flowerTrainingData, x, y)
+  calculateMeanForEveryFlowerProperty(flowerTrainingData)
+  calculateStandardDeviationForEveryFlowerProperty(flowerTrainingData)
+  console.log(flowerTrainingData[2])
 }
 
-const calculateMeanForEveryFlowerProperty = arr => {
-  arr.map(flower =>
+const calculateStandardDeviationForEveryFlowerProperty = flowerTrainingData => {
+  flowerTrainingData.map(flower => {
+    ;(flower.sepal_length_st = st(
+      flower.map(flowerProperty => flowerProperty.sepal_length),
+      flower.sepal_length_mean
+    )),
+      (flower.sepal_width_st = st(
+        flower.map(flowerProperty => flowerProperty.sepal_length),
+        flower.sepal_width_mean
+      )),
+      (flower.petal_length_st = st(
+        flower.map(flowerProperty => flowerProperty.sepal_length),
+        flower.petal_length_mean
+      )),
+      (flower.petal_width = st(
+        flower.map(flowerProperty => flowerProperty.sepal_length),
+        flower.petal_width_mean
+      ))
+  })
+}
+
+const st = (arr, mean) => {
+  const n = arr.length
+
+  return Math.sqrt(
+    arr.map(x => Math.pow(x - mean, 2)).reduce((a, b) => a + b) / n
+  )
+}
+
+const dividedFlowerSetByCategory = (flowerTrainingData, x, y) => {
+  y.map(label => {
+    flowerTrainingData.push(x.filter(x => x.species == label))
+  })
+}
+
+const calculateMeanForEveryFlowerProperty = flowerTrainingData => {
+  flowerTrainingData.map(flower =>
     mean(
       ...flower.map(
         flowerProperty => (
